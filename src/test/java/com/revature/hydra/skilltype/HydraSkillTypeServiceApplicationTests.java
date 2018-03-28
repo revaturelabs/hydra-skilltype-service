c01113300f870d25dc51adbee17f9dcbbbf409f7
Ahmed Awwad <sherman6@cs.washington.edu>
2018-03-28 16:52:03 -0400

    fixed main testing application problem

--- src/test/java/com/revature/hydra/skilltype/HydraSkillTypeServiceApplicationTests.java
+++ src/test/java/com/revature/hydra/skilltype/HydraSkillTypeServiceApplicationTests.java
@@ -3,10 +3,15 @@ package com.revature.hydra.skilltype;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.revature.hydra.test.util.TestContext;
+import com.revature.hydra.test.util.WebAppContext;
+
 @RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = { TestContext.class, WebAppContext.class })
 @SpringBootTest
 public class HydraSkillTypeServiceApplicationTests {
 
