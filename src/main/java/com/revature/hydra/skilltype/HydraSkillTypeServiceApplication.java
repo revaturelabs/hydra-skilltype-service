bc354ab902ca1d8729cba877f10c175b029f8ea5
Ahmed Awwad <sherman6@cs.washington.edu>
2018-03-31 15:05:13 -0400

    added docket bean

--- src/main/java/com/revature/hydra/skilltype/HydraSkillTypeServiceApplication.java
+++ src/main/java/com/revature/hydra/skilltype/HydraSkillTypeServiceApplication.java
@@ -4,7 +4,12 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
@@ -20,4 +25,14 @@ public class HydraSkillTypeServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(HydraSkillTypeServiceApplication.class, args);
 	}
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
+	
 }
